49e5e43a78793543e182a117140ceb76617912de
Bruno Garcia <bruno.garcia.echegaray@gmail.com>
2023-03-25 18:02:12 +0100

    feat: start the web worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./pages/App";
+import { startWorker } from "../mock";
 import "./index.css";
+
+startWorker();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
